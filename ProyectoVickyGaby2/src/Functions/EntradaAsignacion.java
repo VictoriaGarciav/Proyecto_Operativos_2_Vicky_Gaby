35850601c91a2183db1c66bdde365d0c8e4806b7
VictoriaGarciav <113353097+VictoriaGarciav@users.noreply.github.com>
2025-03-16 00:04:37 -0400

    Update Functions EntradaAsignacion 👌

--- ProyectoVickyGaby2/src/Functions/EntradaAsignacion.java
+++ ProyectoVickyGaby2/src/Functions/EntradaAsignacion.java
@@ -4,10 +4,46 @@
  */
 package Functions;
 
-/**
- *
- * @author victoriagarcia
- */
+
 public class EntradaAsignacion {
-    
+    private String nombreArchivo;
+    private int cantidadBloques;
+    private int primerBloque;
+
+    public EntradaAsignacion(String nombreArchivo, int cantidadBloques, int primerBloque) {
+        this.nombreArchivo = nombreArchivo;
+        this.cantidadBloques = cantidadBloques;
+        this.primerBloque = primerBloque;
+    }
+
+    // Getters y Setters
+    public String getNombreArchivo() {
+        return nombreArchivo;
+    }
+
+    public void setNombreArchivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    public int getCantidadBloques() {
+        return cantidadBloques;
+    }
+
+    public void setCantidadBloques(int cantidadBloques) {
+        this.cantidadBloques = cantidadBloques;
+    }
+
+    public int getPrimerBloque() {
+        return primerBloque;
+    }
+
+    public void setPrimerBloque(int primerBloque) {
+        this.primerBloque = primerBloque;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Archivo: " + nombreArchivo);
+        System.out.println("Bloques asignados: " + cantidadBloques);
+        System.out.println("Primer Bloque: " + primerBloque);
+    }
 }
